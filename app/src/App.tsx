050cd658d6aee001cc858c9655a274fc5e824c86
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-04-04 22:26:15 +0300

    update test application

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,7 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Modal } from "../../src";
 
-const modalStyles: React.CSSProperties = {
-  background: "#ffffff",
-  padding: "10px",
-  borderRadius: "10px",
-  boxShadow: "0 0 18px rgba(0, 0, 0, 0.2)",
-};
+import "./App.css";
 
 export default function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,11 +10,17 @@ export default function App() {
   const closeModal = () => setIsOpen(false);
 
   return (
-    <div>
+    <div className="root">
       <h1>app w/ modal.jsx</h1>
       <button onClick={openModal}>open modal</button>
 
-      <Modal style={modalStyles} isOpen={isOpen} onClickOutside={closeModal}>
+      <Modal
+        className="modal"
+        backdropClassName="backdrop"
+        isOpen={isOpen}
+        onEscapeDown={closeModal}
+        onClickOutside={closeModal}
+      >
         <h2>modal.jsx</h2>
         <p>just a sample text</p>
         <button onClick={closeModal}>close modal</button>
